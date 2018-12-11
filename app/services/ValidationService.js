b9c9f78e0dd0b3631b980a1b149f4121a54a9b43
devnulled <devnulled@cryptaz.org>
2018-12-12 03:00:12 +0700

    ValidationService: validateRegisterErrorRequest

--- app/services/ValidationService.js
+++ app/services/ValidationService.js
@@ -14,6 +14,32 @@ class ValidationService {
         return Joi.validate(registerControllerRequest, schema)
 
     }
+
+    static validateRegisterErrorRequest(registerErrorRequest) {
+
+        const schema = Joi.object().keys({
+            UID: Joi.string().required(),
+            Key: Joi.string().required(),
+            ErrorTime: Joi.number().required(),
+            Msg: Joi.string().required(),
+        })
+
+        const validationResult = Joi.validate(registerErrorRequest, schema)
+
+        try {
+            const date = new Date(registerErrorRequest.ErrorTime)
+
+            if (!date instanceof Date || date.getFullYear() < 2000) {
+                throw new Error()
+            }
+
+        } catch (e) {
+            validationResult.error = true
+        }
+
+        return validationResult
+    }
+
 }
 
 module.exports = ValidationService
