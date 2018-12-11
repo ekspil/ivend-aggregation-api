f82b3d6d2d34290078773fc7ac45de4da6cb0fbf
devnulled <devnulled@cryptaz.org>
2018-12-12 02:59:41 +0700

    ControllerService: addErrorToController

--- app/services/ControllerService.js
+++ app/services/ControllerService.js
@@ -1,4 +1,5 @@
 const ControllerModel = require("../models/mongoose/models/ControllerModel")
+const ControllerErrorModel = require("../models/mongoose/models/ControllerErrorModel")
 
 /**
  * ControllerService, maintains {Controller} entity
@@ -7,10 +8,11 @@ class ControllerService {
 
     constructor() {
         this.getControllerByUID = this.getControllerByUID.bind(this)
+        this.addErrorToController = this.addErrorToController.bind(this)
     }
 
     /**
-     * Creates {Bill}
+     * Creates {Controller}
      * @param UID {string}
      * @returns {Promise<ControllerModel>}
      */
@@ -21,6 +23,28 @@ class ControllerService {
             })
 
         return await query.findOne().exec()
+    }
+
+
+    /**
+     * Adds error to controller
+     * @param registerErrorRequest {RegisterErrorRequest}
+     * @returns {Promise<ControllerModel>}
+     */
+    async addErrorToController(registerErrorRequest) {
+        const errorTime = new Date(registerErrorRequest.ErrorTime)
+        const {UID} = registerErrorRequest
+
+        const controller = await ControllerModel
+            .find({
+                uid: UID
+            })
+            .findOne()
+            .exec()
+
+        controller.controllerErrors.push(new ControllerErrorModel({errorTime, message: registerErrorRequest.Msg}));
+
+        return await controller.save()
     }
 }
 
