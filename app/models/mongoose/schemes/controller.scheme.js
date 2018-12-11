fef016cfd2b1b4501633a37822adb92947054aeb
devnulled <devnulled@cryptaz.org>
2018-12-11 21:30:22 +0700

    ControllerScheme: add more required fields

--- app/models/mongoose/schemes/controller.scheme.js
+++ app/models/mongoose/schemes/controller.scheme.js
@@ -1,5 +1,17 @@
 const mongoose = require("mongoose")
 
 module.exports = new mongoose.Schema({
-    uid: String,
+    uid: {
+        type: String,
+        required: true
+    },
+    accessKey: {
+        type: String,
+        required: true
+    },
+    mode: {
+        type: String,
+        required: true,
+        enum: ['mdb', 'exe', 'cashless']
+    },
 })
