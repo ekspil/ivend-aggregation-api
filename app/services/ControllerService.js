f4610ba21012eaddb69b265a162472252c162931
pshenmic <pshenmic@gmail.com>
2019-02-01 02:13:39 +0700

    ControllerService: add registerState method impl

--- app/services/ControllerService.js
+++ app/services/ControllerService.js
@@ -15,6 +15,7 @@ class ControllerService {
         this.getControllerByUID = this.getControllerByUID.bind(this)
         this.authController = this.authController.bind(this)
         this.addErrorToController = this.addErrorToController.bind(this)
+        this.registerState = this.registerState.bind(this)
     }
 
     /**
@@ -28,9 +29,11 @@ class ControllerService {
           controller: getControllerByUID(uid: "${UID}") {
             uid
             mode
+            accessKey
           }
         }
         `
+
 
         const data = await client.request(query)
 
@@ -64,6 +67,58 @@ class ControllerService {
         }
 
         return data.authController
+    }
+
+
+    /**
+     * Creates {Controller}
+     * @param registerStateRequest {RegisterStateRequest}
+     * @returns {Promise<ControllerModel>}
+     */
+    async registerState(registerStateRequest) {
+        const query = `
+        mutation($input: ControllerStateInput) {
+          registerControllerState(input: $input) {
+            id
+          }
+        }
+        `
+
+        const {ch, bh, cv, bv, bus, ms} = registerStateRequest.State
+        const [dex1Status, dex2Status, exeStatus, mdbStatus] = bus.split("")
+
+        const BusStatusMap = {
+            "0": "DISABLED",
+            "1": "OK",
+            "2": "ERROR",
+        }
+
+        const SignalStrengthMap = {
+            "0": "BAD",
+            "1": "MEDIUM",
+            "2": "GOOD",
+        }
+
+        const variables = {
+            input: {
+                controllerUid: registerStateRequest.UID,
+                coinAcceptorStatus: BusStatusMap[ch],
+                billAcceptorStatus: BusStatusMap[bh],
+                coinAmount: cv,
+                billAmount: bv,
+                dex1Status: BusStatusMap[dex1Status],
+                dex2Status: BusStatusMap[dex2Status],
+                exeStatus: BusStatusMap[exeStatus],
+                mdbStatus: BusStatusMap[mdbStatus],
+                signalStrength: SignalStrengthMap[ms]
+            }
+        }
+
+        const data = await client.request(query, variables)
+
+        if (!data.registerControllerState) {
+            throw new Error("Failed to update controller state, registerControllerState returned null")
+        }
     }
 
 
