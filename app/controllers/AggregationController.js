b33306835bb450cd516bf9c0e52ffd227b19b0cd
pshenmic <pshenmic@gmail.com>
2019-04-19 19:44:02 +0700

    AggregationController: added check for machine not found to registerState

--- app/controllers/AggregationController.js
+++ app/controllers/AggregationController.js
@@ -111,6 +111,20 @@ class AggregationController {
             ctx.status = 200
         }
         catch (e) {
+            if (e && e.response && Array.isArray(e.response.errors) && e.response.errors[0] && e.response.errors[0].message) {
+                const {message} = e.response.errors[0]
+                if (message === "Controller not found") {
+                    return this.returnNotFound(ctx)
+                }
+            }
+
+            if (e && e.response && Array.isArray(e.response.errors) && e.response.errors[0] && e.response.errors[0].message) {
+                const {message} = e.response.errors[0]
+                if (message === "Machine not found") {
+                    return this.returnMachineNotFound(ctx)
+                }
+            }
+
             return this.returnInternalServerError(ctx, e)
         }
 
