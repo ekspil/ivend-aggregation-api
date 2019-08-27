95f8b483ef337100b7f2de68e00b130aa59674ab
pshenmic <pshenmic@gmail.com>
2019-08-27 18:21:13 +0700

    RegisterEventRequest: add model for registerEvent

---
+++ app/models/RegisterEventRequest.js
@@ -0,0 +1,12 @@
+class RegisterEventRequest {
+
+  constructor(rawObj) {
+    const {UID, Key, EventTime, Code} = rawObj
+    this.UID = UID || null
+    this.Key = Key || null
+    this.EventTime = EventTime || null
+    this.Code = Code || null
+  }
+}
+
+module.exports = RegisterEventRequest
