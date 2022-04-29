f11dc6560cb13405feef6e6003f6cabd0cbdd5a8
Efremov AV <ekspil@yandex.ru>
2022-04-29 18:18:23 +1000

    telemetron

--- app/models/TelemetronEventRequest.js
+++ app/models/TelemetronEventRequest.js
@@ -1,7 +1,7 @@
 class TelemetronEventRequest {
 
     constructor(rawObj) {
-        const {imei, time, s, mdb, iccid, reason, qlt, bat, exe, mdb_product} = rawObj
+        const {imei, time, s, mdb, iccid, reason, qlt, bat, exe, mdb_product, version, simnumber} = rawObj
         this.imei = imei || null
         this.time = time || null
         this.s = s || null
@@ -11,6 +11,8 @@ class TelemetronEventRequest {
         this.qlt = qlt || null
         this.bat = bat || null
         this.exe = exe || null
+        this.version = version || null
+        this.simnumber = simnumber || null
         this.mdb_product = mdb_product || null
     }
 }
